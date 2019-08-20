20b00d6e439e8bdc41d309f440001b1f5226490f
lusalga <ladsaldanha@gmail.com>
2019-08-20 18:10:48 -0500

    button enable to pick a category, fetching data needed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './index.css';
+import CardList from './CardList';
+import Button from './Button';
+import Footer from './Footer';
+import Nav from './Nav';
+import Cover from './Cover';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      info: [],
+      urls: []
+    }
+    this.handleEvent = this.handleEvent.bind(this)
+  }
+
+   componentDidMount(){
+    const urlList = ['https://swapi.co/api/'];
+    fetch(urlList)
+      .then(resp => resp.json())
+      .then(data => {
+        // console.log('data:', data);
+        this.setState({
+          urls: data
+        })
+      });
+   }
+
+   handleEvent(event){
+      const { name } = event.target;
+      this.setState({
+         info: [] 
+      });
+      for (let [key, value] of Object.entries(this.state.urls)){
+         if (name === key.toString()){
+            // console.log(`${key}`);
+            fetch(value)
+               .then(resp => resp.json())
+               .then(data => {
+                  this.setState({info: this.state.info.concat(data.results)})
+               })
+        }
+      }
+   }
+
+    render(){
+        return (
+            <div> 
+              <Nav />
+              <Cover />
+              <Button onSubmit={this.handleEvent} />
+              <CardList info={this.state.info} />
+              <Footer />
+            </div>
+        )
+    }
 }
 
 export default App;
+
+   
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => {
+	return (
+		<nav className="navBox">
+		  	<ul class="main-nav">
+		  		<li><a href="#">About</a></li>
+		  		<li><a href="#">Products</a></li>
+		  		<li><a href="#">Our Team</a></li>
+		  		<li class="push"><a href="">Contact</a></li>
+		  	</ul>
+		</nav>
+	)
+}
+
+export default Nav;
