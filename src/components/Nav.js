2d990c958110c60a82a3ac272851f5d9e42de0eb
lusalga <ladsaldanha@gmail.com>
2020-03-24 20:09:50 -0500

    fixed console error regarding className in Nav.js component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Nav = () => {
 	return (
 		<nav className="navBox">
-		  	<ul class="main-nav">
+		  	<ul className="main-nav">
 		  		<li><a href="#about">About</a></li>
 		  		<li><a href="#product">Products</a></li>
 		  		<li><a href="#team">Our Team</a></li>
-		  		<li class="push"><a href="#contact">Contact</a></li>
+		  		<li className="push"><a href="#contact">Contact</a></li>
 		  	</ul>
 		</nav>
 	)
